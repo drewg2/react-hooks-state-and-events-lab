b5a817abd55c92ddc5749f333e716b155edc0393
Drew Grey <dtsgrey@gmail.com>
2022-03-06 18:17:43 -0600

    commit tree 9772766bfd807829805347e1d7a2cb5b414e7336

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const [inCart, setInCart] = useState(false)
+  const cartName = inCart ? "in-cart" : ""
+  const buttonName = inCart ? "Remove From Cart" : "Add to Cart"
+
+  function handleAdd() {
+    setInCart((inCart) => (!inCart))
+  }
+  
   return (
-    <li className="">
+    <li className={cartName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAdd}>{buttonName}</button>
     </li>
   );
 }
